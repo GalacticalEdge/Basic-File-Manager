08b4eb4318f58ca4e00c0f592235f7cb436e8212
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2024-01-05 11:43:24 -0800

    Add files via upload

--- BasicFileManager.py
+++ BasicFileManager.py
@@ -1,13 +1,14 @@
 import shutil as st # This will be needed once we start working on actual file operations
 import pathlib as pl
 import tabulate as tbl
+import subprocess as sbp
 
-path = pl.Path("/")
+path = pl.Path.home()
 
 while True:
-    action = input(">>>")
+    action = input(str(path) + " ")
     if action == "help" or action == "h":
-        print("\"help\" and \"h\": Brings up this\n\"list\" and \"ls\": List files in the current directory\n\"exists\" and \"ex\": Check if a directory or file exists\n\"changedir\" and \"cd\": Change the directory. Please be sure to enter the command followed by the directory seperated by a space\n\"copy\": Copy a file to another file or directory")
+        print(tbl.tabulate([["\"help\" or \"h\"", "Brings up this"], ["\"list\" and \"ls\"", "List files in the current directory"], ["\"exists\" and \"ex\"", "Check if a directory or file exists. Please be sure to enter the command followed by the directory seperated by a space. If entering a new directory connected to the current directory, put a \"~\" character right before the path. Ex. \"~/path/to/directory\""], ["\"changedir\" and \"cd\"", "Change the directory. Please be sure to enter the command followed by the directory seperated by a space. If entering a new directory connected to the current directory, put a \"~\" character right before the path. Ex. \"~/path/to/directory/\""], ["\"run\"", "Runs a new process in the current directory. Arguments come after the \"run\" command. Ex. run [example]"]], headers=["Command", "Purpose"]))
     elif action == "list" or action == "ls":
         FileTable = []
         for x in path.iterdir():
@@ -22,8 +23,8 @@ while True:
         FileTable.sort()
         print(tbl.tabulate(FileTable, headers=["Name", "File Type", "Complete Path"]))
     elif action[:action.find(" ")] == "exists" or action[0:action.find(" ")] == "ex":
-        if pl.Path(str(path) + "/" + action[action.find(" ") + 1:]).is_dir():
-            directory = pl.Path(str(path) + "/" + action[action.find(" ") + 1:])
+        if action[action.find(" ") + 1] == "~" and pl.Path(str(path) + "/" + action[action.find(" ") + 2:]).is_dir():
+            directory = pl.Path(str(path) + "/" + action[action.find(" ") + 2:])
         else:
             directory = pl.Path(action[action.find(" ") + 1:])
         if pl.Path(directory).exists():
@@ -32,13 +33,35 @@ while True:
             elif (pl.Path(directory).is_file()):
                 print("The file " + str(directory) + " exists")
         else:
-            print(directory + " does not exist or the command is incomplete")
+            print(str(directory) + " does not exist or the command is incomplete")
     elif action[:action.find(" ")] == "cd" or action[0:action.find(" ")] == "changedir":
-        if pl.Path(str(path) + "/" + action[action.find(" ") + 1:]).is_dir():
-            path = pl.Path(str(path) + "/" + action[action.find(" ") + 1:])
+        if action[action.find(" ") + 1] == "~" and pl.Path(str(path) + "/" + action[action.find(" ") + 2:]).is_dir():
+            path = pl.Path(str(path) + "/" + action[action.find(" ") + 2:])
         elif pl.Path(action[action.find(" ") + 1:]).is_dir():
             path = pl.Path(action[action.find(" ") + 1:])
         else:
             print("Please enter a a valid path")
+    elif action[:action.find(" ")] == "run":
+        if action.find("sudo") != -1:
+            confirm_operation = True
+            print("You are going to execute a command in sudo mode. This is a potentially dangerous operation that can cause damage to your files or computer. Are you sure you want to continue? ", end="")
+            while True:
+                user_input = input("\"Y/y\" for yes or \"N/n\" for no: ")
+                if user_input.lower() == "y":
+                    confirm_operation = True
+                    break
+                elif user_input.lower() == "n":
+                    confirm_operation = False
+                    break
+                else:
+                    print("Please enter a valid option")
+                    continue
+        if confirm_operation:
+            try:
+                sbp.run([str(path), action[action.find(" ") + 1:]])
+            except Exception as e:
+                print("An error occurred in the subprocess. " + "Error: " + str(e))
+        else:
+            print("Operation aborted")
     else:
         print("The entered input is invalid or incomplete. Type \"help\" or \"h\" for more information")
